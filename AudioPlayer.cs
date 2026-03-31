f4174ebaffb02d146f9e9081a024dba7165c3528
10477704 <ST10477704@vcconnect.edu.za>
2026-03-31 08:05:49 +0200

    Added AudioPlayer for the voice greeting functionality

---
+++ AudioPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Media;
+using System.IO;
+
+namespace CybersecurityChatbot
+{
+    public static class AudioPlayer
+    {
+        public static void PlayGreeting()
+        {
+            string soundFile = "greeting.wav";
+
+            if (File.Exists(soundFile))
+            {
+                try
+                {
+                    using (SoundPlayer player = new SoundPlayer(soundFile))
+                    {
+                        player.PlaySync();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Note: Could not play voice greeting.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Note: Voice greeting file not found. Text greeting only.");
+            }
+        }
+    }
+}
